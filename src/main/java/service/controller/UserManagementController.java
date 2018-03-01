0c600993ee17d7e946ac10770be7d03072f1045e
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-03-01 23:04:50 +0800

    Daily update from earth, change Spring Boot version to 2.0.0RELEASE

--- src/main/java/service/controller/UserManagementController.java
+++ src/main/java/service/controller/UserManagementController.java
@@ -1,12 +1,19 @@
 package service.controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class UserManagementController {
     @RequestMapping("/login")
     public String login(){
+        return "hello world";
+    }
 
+    public static void main(String args[]) throws Exception{
+        SpringApplication.run(UserManagementController.class, args);
     }
 }
