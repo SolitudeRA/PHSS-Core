c8e6ea7856d675ae2894c7425f2e6b72b4df7753
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-07-14 19:40:11 +0800

    Update from earth, dependencies fix

--- src/main/java/me/protogalaxy/PhssApplication.java
+++ src/main/java/me/protogalaxy/PhssApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
+//TODO:change groupid
 @SpringBootApplication
 @EnableConfigurationProperties({PhssStorageServiceConfig.class})
 @Import(PhssDatasourceConfig.class)
