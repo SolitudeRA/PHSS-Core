ce22ab394367f84733af85453ad8dbb74173e817
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-07-18 23:16:28 +0800

    Update from earth

--- src/main/java/org/protogalaxy/phss/PhssApplication.java
+++ src/main/java/org/protogalaxy/phss/PhssApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
-
+//TODO:番组管理，视频管理，图书管理
 @SpringBootApplication
 @EnableConfigurationProperties({PhssStorageServiceConfig.class})
 @Import(PhssDatasourceConfig.class)
