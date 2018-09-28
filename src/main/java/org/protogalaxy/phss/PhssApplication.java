67297664d08c1944339e71066ba262e1d66406dc
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-09-28 20:59:00 +0800

    Update from earth, daily update

--- src/main/java/org/protogalaxy/phss/PhssApplication.java
+++ src/main/java/org/protogalaxy/phss/PhssApplication.java
@@ -4,6 +4,7 @@ import org.protogalaxy.phss.service.config.PhssStorageServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
 @EnableConfigurationProperties({PhssStorageServiceConfig.class})
