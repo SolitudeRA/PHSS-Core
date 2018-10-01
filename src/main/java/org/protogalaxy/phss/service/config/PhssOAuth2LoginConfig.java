ce3aba5320d7911a5eaf93f263c7868e0a8be50b
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-10-01 14:17:12 +0800

    Update from earth, OAuth2 login update, dependencies update, not completed

--- src/main/java/org/protogalaxy/phss/service/config/PhssOAuth2LoginConfig.java
+++ src/main/java/org/protogalaxy/phss/service/config/PhssOAuth2LoginConfig.java
@@ -2,6 +2,8 @@ package org.protogalaxy.phss.service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
@@ -11,22 +13,27 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 @Configuration
 public class PhssOAuth2LoginConfig {
     @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(){
+    public OAuth2AuthorizedClientService auth2AuthorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(this.clientRegistrationRepository());
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
         return new InMemoryClientRegistrationRepository(this.bangumiClientRegistration());
     }
 
-    private ClientRegistration bangumiClientRegistration(){
+    private ClientRegistration bangumiClientRegistration() {
         return ClientRegistration.withRegistrationId("bangumi")
-                .clientId("bgm6165b9e794a763e1")
-                .clientSecret("48aca6275eb4259de87406ec96120e34")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUriTemplate("http://localhost:8080/login/oauth2/code/bangumi")
-                .scope("")
-                .authorizationUri("https://bgm.tv/oauth/authorize")
-                .tokenUri("https://bgm.tv/oauth/access_token")
-                .userInfoUri("https://bgm.tv/oauth/token_status")
-                .clientName("Bangumi")
-                .build();
+                                 .clientId("bgm6165b9e794a763e1")
+                                 .clientSecret("48aca6275eb4259de87406ec96120e34")
+                                 .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                                 .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+                                 .scope("")
+                                 .authorizationUri("https://bgm.tv/oauth/authorize")
+                                 .tokenUri("https://bgm.tv/oauth/access_token")
+                                 .userInfoUri("https://bgm.tv/oauth/token_status")
+                                 .clientName("Bangumi")
+                                 .build();
     }
 }
