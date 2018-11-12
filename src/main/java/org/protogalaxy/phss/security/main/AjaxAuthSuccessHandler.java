20874a0dea9a24b06c782e6e337ee5980ce92685
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-11-12 22:08:00 +0800

    Update from earth, metadata services update

--- src/main/java/org/protogalaxy/phss/security/main/AjaxAuthSuccessHandler.java
+++ src/main/java/org/protogalaxy/phss/security/main/AjaxAuthSuccessHandler.java
@@ -2,6 +2,7 @@ package org.protogalaxy.phss.security.main;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.protogalaxy.phss.datasource.entity.user.UserEntity;
+import org.protogalaxy.phss.datasource.resource.main.user.UserResource;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,10 +18,11 @@ import java.io.IOException;
 public class AjaxAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        UserEntity userDetails = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         ObjectMapper mapper = new ObjectMapper();
+        UserEntity userDetails = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        response.setStatus(200);
         response.setCharacterEncoding("UTF-8");
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().print(mapper.writeValueAsString(userDetails));
+        response.getWriter().print(mapper.writeValueAsString(new UserResource(userDetails)));
     }
 }
