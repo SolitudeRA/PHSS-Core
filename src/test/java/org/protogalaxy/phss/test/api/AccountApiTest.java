9a774f5a91264d54a65ea3f8dfdabac4f890a18b
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-12-05 22:05:35 +0800

    Update from earth, rest doc generator update

--- src/test/java/org/protogalaxy/phss/test/api/AccountApiTest.java
+++ src/test/java/org/protogalaxy/phss/test/api/AccountApiTest.java
@@ -1,32 +1,38 @@
 package org.protogalaxy.phss.test.api;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.protogalaxy.phss.controller.AccountController;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @RunWith(SpringRunner.class)
-@WebMvcTest(AccountController.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
 public class AccountApiTest {
     private static final String urlBase = "/account";
@@ -34,21 +40,26 @@ public class AccountApiTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentationContextProvider) {
+    public void setUp(RestDocumentationContextProvider restDocumentationContextProvider) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                                       .apply(documentationConfiguration(restDocumentationContextProvider))
+                                      .alwaysDo(document("{ClassName}/{step}"))
                                       .build();
     }
 
     @Test
-    public void register() throws Exception {
+    @Rollback(value = false)
+    public void testCase_1_register() throws Exception {
         this.mockMvc.perform(post(urlBase + "/register")
                                      .param("username", "alpha")
                                      .param("password", "test")
                                      .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andDo(document("Register account",
+                    .andDo(document("register-account",
                                     requestParameters(
                                             parameterWithName("username").description("Name of the account"),
                                             parameterWithName("password").description("Password of the account")
@@ -56,9 +67,50 @@ public class AccountApiTest {
     }
 
     @Test
-    public void getAccount() throws Exception {
-        this.mockMvc.perform(get(urlBase + "{username}", "alpha"))
+    @WithMockUser(username = "alpha", password = "test")
+    public void testCase_2_getAccount() throws Exception {
+        this.mockMvc.perform(get(urlBase + "/{username}", "alpha"))
                     .andExpect(status().isOk())
-                    .andDo(document("Get account with username", pathParameters(parameterWithName("username").description("Name of the account"))));
+                    .andDo(document("get-account-with-username", pathParameters(parameterWithName("username").description("Name of the account"))));
+    }
+
+    @Test
+    @WithMockUser(username = "alpha", password = "test")
+    public void testCase_3_disableAccount() throws Exception {
+        this.mockMvc.perform(patch(urlBase + "/{username}/disable", "alpha"))
+                    .andExpect(status().isOk())
+                    .andDo(document("disable-account", pathParameters(parameterWithName("username").description("Name of the account"))));
+    }
+
+    @Test
+    @WithMockUser(username = "alpha", password = "test")
+    public void testCase_4_enableAccount() throws Exception {
+        this.mockMvc.perform(patch(urlBase + "/{username}/enable", "alpha"))
+                    .andExpect(status().isOk())
+                    .andDo(document("enable-account", pathParameters(parameterWithName("username").description("Name of the account"))));
+    }
+
+    @Test
+    @WithMockUser(username = "alpha", password = "test")
+    public void testCase_5_lockAccount() throws Exception {
+        this.mockMvc.perform(patch(urlBase + "/{username}/lock", "alpha"))
+                    .andExpect(status().isOk())
+                    .andDo(document("lock-account", pathParameters(parameterWithName("username").description("Name of the account"))));
+    }
+
+    @Test
+    @WithMockUser(username = "alpha", password = "test")
+    public void testCase_6_unlockAccount() throws Exception {
+        this.mockMvc.perform(patch(urlBase + "/{username}/unlock", "alpha"))
+                    .andExpect(status().isOk())
+                    .andDo(document("unlock-account", pathParameters(parameterWithName("username").description("Name of the account"))));
+    }
+
+    @Test
+    @WithMockUser(username = "alpha", password = "test")
+    public void testCase_7_expireAccount() throws Exception {
+        this.mockMvc.perform(patch(urlBase + "/{username}/expire", "alpha"))
+                    .andExpect(status().isOk())
+                    .andDo(document("expire-account", pathParameters(parameterWithName("username").description("Name of the account"))));
     }
 }
--- src/test/java/org/protogalaxy/phss/test/api/MusicApiTest.java
+++ src/test/java/org/protogalaxy/phss/test/api/MusicApiTest.java
@@ -1,15 +1,14 @@
 package org.protogalaxy.phss.test.api;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.protogalaxy.phss.controller.filesystem.MusicController;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -18,23 +17,24 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import javax.servlet.http.Cookie;
-
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
 public class MusicApiTest {
     private static final String urlBase = "/music";
@@ -46,7 +46,7 @@ public class MusicApiTest {
     private WebApplicationContext webApplicationContext;
 
     @BeforeEach
-    public void setUp(RestDocumentationContextProvider restDocumentationContextProvider) throws Exception {
+    public void setUp(RestDocumentationContextProvider restDocumentationContextProvider) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                                       .apply(documentationConfiguration(restDocumentationContextProvider))
                                       .build();
@@ -54,30 +54,31 @@ public class MusicApiTest {
 
     @Test
     @WithMockUser(username = "alpha", password = "test")
-    public void index() throws Exception {
-        this.mockMvc.perform(get(urlBase).accept(MediaType.APPLICATION_JSON))
+    public void testCase_1_uploadTrack() throws Exception {
+        this.mockMvc.perform(fileUpload(urlBase + "/upload")
+                                     .file("track", Files.readAllBytes(Paths.get("src/test/resources/files/MEGALOBOX.aiff").toAbsolutePath()))
+                                     .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andDo(document("music",
-                                    links(halLinks(),
-                                          linkWithRel("upload").description("Upload a track"),
-                                          linkWithRel("multiupload").description("Upload tracks"),
-                                          linkWithRel("album").description("List user album"),
-                                          linkWithRel("album/{uuid}").description("Get an album with UUID"))
-                    ));
+                    .andDo(document("upload-album", responseFields(
+                            fieldWithPath("")
+                    )));
     }
 
     @Test
     @WithMockUser(username = "alpha", password = "test")
-    public void uploadTrack() throws Exception {
-        this.mockMvc.perform(multipart(urlBase + "/upload").file("track", "test".getBytes())
-                                                           .accept(MediaType.APPLICATION_JSON))
+    public void testCase_2_uploadTracks() throws Exception {
+        this.mockMvc.perform(fileUpload(urlBase + "/multiupload")
+                                     .file("tracks", Files.readAllBytes(Paths.get("src/test/resources/files/MEGALOBOX.aiff").toAbsolutePath()))
+                                     .file("tracks", Files.readAllBytes(Paths.get("src/test/resources/files/ちいさな冒険者.aiff").toAbsolutePath()))
+                                     .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andDo(document("upload-album"));
+                    .andDo(document("upload-albums"));
     }
+
 
     @Test
     @WithMockUser(username = "alpha", password = "test")
-    public void listUserAlbum() throws Exception {
+    public void testCase_3_listUserAlbum() throws Exception {
         this.mockMvc.perform(get(urlBase + "/album").accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
                     .andDo(document("list-user-album"));
@@ -85,9 +86,17 @@ public class MusicApiTest {
 
     @Test
     @WithMockUser(username = "alpha", password = "test")
-    public void getAlbumByUUID() throws Exception {
+    public void testCase_4_getAlbum() throws Exception {
         this.mockMvc.perform(get(urlBase + "/album/").accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andDo(document("get-album-by-uuid"));
+                    .andDo(document("get-album"));
+    }
+
+    @Test
+    @WithMockUser(username = "alpha", password = "test")
+    public void testCase_5_updateAlbum() throws Exception {
+        this.mockMvc.perform(patch(urlBase + "/album/{uuid}/update", "").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andDo(document("update-album"));
     }
 }
